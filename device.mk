63f8b4de093cdc4dec8924f95a1209996d14f9fd
MLZ94 <zugutoroberto@gmail.com>
2021-04-11 08:31:41 -0400

    X00QD: revert useless commits
    Revert "X00QD: Add Dolby audio"
    
    This reverts commit d5bd3298927968263f52f3a5beec85b8c2624ae6.
    
    Revert "X00QD: overlay: Set Google autofill service as default"
    
    This reverts commit c770259b71b4fbcf0700c3cb80a323f61292419c.
    
    Revert "X00QD: Add Snap config overlay"
    
    This reverts commit 7d3f9d26d4fa0b8d21decc269ea3ffb7d5d736ae.
    
    Revert "X00QD: Downgrade SoundTrigger to 2.1"
    
    This reverts commit 774a4973974feb3784f27f63f35e0c782fc4593a.
    
    Revert "X00QD: Make the UI smoother"
    
    This reverts commit 094ec1f8037b980232293ec84b8c0b3eaa349cb4.

--- device.mk
+++ device.mk
@@ -82,7 +82,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.effect@2.0-service \
-    android.hardware.soundtrigger@2.1-impl \
+    android.hardware.soundtrigger@2.2-impl \
     android.hardware.audio.common@2.0-util \
     android.hardware.audio.common@5.0-util \
     audio.a2dp.default \
@@ -329,8 +329,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles.xml \
     $(LOCAL_PATH)/configs/media/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
     $(LOCAL_PATH)/configs/media/media_profiles_sdm660_v1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_sdm660_v1.xml \
-    $(LOCAL_PATH)/configs/media/media_profiles_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_vendor.xml \
-    $(LOCAL_PATH)/configs/media/media_codecs_dolby_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_dolby_audio.xml
+    $(LOCAL_PATH)/configs/media/media_profiles_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_vendor.xml
 
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
