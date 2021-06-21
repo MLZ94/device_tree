MLZ94/android_device_asus_X00QD

X00QD: Import AdaptLaunch and some other perf * From LA.UM.8.6.2.r1-08600-89xx.0
Test: Better app launching time

Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>
Signed-off-by: clarencelol <clarencekuiek@icloud.com>

device.mk
@@ -507,6 +507,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
+# Adapt Launch 
+PRODUCT_COPY_FILES += \
+$(COMMON_PATH)/configs/lm/AdaptLaunchFeature.xml:$(TARGET_COPY_OUT_VENDOR)/etc/lm/AdaptLaunchFeature.xml \
+
 # Touchscreen
 PRODUCT_PACKAGES += \
     libtinyxml2

MLZ94/device_tree

device.mk
@@ -513,6 +513,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
+# Adapt Launch 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/lm/AdaptLaunchFeature.xml:$(TARGET_COPY_OUT_VENDOR)/etc/lm/AdaptLaunchFeature.xml \
+
 # Touchscreen
 PRODUCT_PACKAGES += \
     libtinyxml2